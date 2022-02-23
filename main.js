d4d98b41dbdb7f262cdc8b48f7525f8ab856d975
Dana <danamudrova@gmail.com>
2022-02-23 21:29:56 +0100

    set starting coordinates and move coinhunter on that (onload event)

--- main.js
+++ main.js
@@ -7,3 +7,24 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 
 
 // sem začni psát svůj program
+	
+	
+
+	// //panáček
+	
+	
+
+	function start (){
+ 		let startTop = '800px' //startovní souřadnice
+		let startLeft = '1000px' 
+		let hunter = document.querySelector('#panacek');
+		hunter.style.top = startTop; //umístění panáčka
+		hunter.style.left = startLeft;
+
+	}
+
+
+	
+	// hunter.style.left = 'movementLeft()' + 'px'; měnící se pozice podle šipek
+	// hunter.style.top = 'movementTop()' + 'px'; měnící se pozice podle šipek
+	
