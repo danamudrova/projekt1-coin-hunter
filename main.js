7014534f357f8f884692d3126811d63c90c51560
Dana <danamudrova@gmail.com>
2022-03-03 22:31:28 +0100

    panacek borders stops him + random coin appears

--- main.js
+++ main.js
@@ -3,54 +3,62 @@
 
 // sem začni psát svůj program
 
-//function start(){
-//	document.getElementById('hudba').play()
-//}
+// mince
+function showCoin(){
+	let coin = document.getElementById('mince');
+	let coinX = Math.floor(Math.random()*900)
+	let coinY = Math.floor(Math.random()*1500)
 
-function pressArrow(udalost){
-	
-	let hunter = document.getElementById('panacek');
-	let hunterX =  window.getComputedStyle (hunter,null).getPropertyValue('top')
-	let hunterY =  window.getComputedStyle (hunter,null).getPropertyValue('left')
-	let currentPositionX = parseInt(hunterX);
-	let currentPositionY = parseFloat(hunterY);
-
-//window.innerWidth = 0
-//window.innerHeight = 0
-	
-	if (udalost.key=='ArrowLeft') {
-				hunter.style.left = (currentPositionY - 10) + 'px';
-				hunter.src = 'obrazky/panacek-vlevo.png'
-			}
- 		else if (udalost.key=='ArrowRight'){
-			 	hunter.style.left = (currentPositionY + 10) + 'px';
-				hunter.src = 'obrazky/panacek-vpravo.png'
-			}
-	 	else if (udalost.key=='ArrowUp'){
-			 	hunter.style.top = (currentPositionX - 10) + 'px';
-				hunter.src = 'obrazky/panacek-nahoru.png'
-			}
-		else if (udalost.key=='ArrowDown'){
-				hunter.style.top = (currentPositionX + 10) + 'px';
-				hunter.src = 'obrazky/panacek.png'
-			}
-	 	
-	else {alert('Použij šipky na ovládání panáčka')
-	}  
-	
-	if (hunter.style.left < 0){ hunter.style.left = (cureentPositionY) + 'px'}  
-	
+	coin.style.left = coinY +'px';
+	coin.style.top = coinX +'px';
 }
-
-function coinAppear () {
-	let coin = getElementById('mince');
-	let minceX = Math.random('top');
-	let minceY = Math.random('left');
-
 // toto budeš potřebovat později
 /*
 if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || panacekY + panacekVyska < minceY || minceY + minceVyska < panacekY)) {
 	// panacek a mince se prekryvaji
 }
 */
+
+
+// panáček 
+function pressArrow(udalost){
+	document.getElementById('hudba').play()
+
+	let hunter = document.getElementById('panacek');
+	let hunterX =  window.getComputedStyle (hunter,null).getPropertyValue('top')
+	let hunterY =  window.getComputedStyle (hunter,null).getPropertyValue('left')
+	let currentPositionX = parseInt(hunterX);
+	let currentPositionY = parseFloat(hunterY);
+    let width = window.innerWidth
+	let height = window.innerHeight
+
+	if (udalost.key=='ArrowLeft'&& hunterY >= '0px') {
+				hunter.style.left = (currentPositionY - 10) + 'px';
+				hunter.src = 'obrazky/panacek-vlevo.png'
+				
+			}
+ 		else if (udalost.key=='ArrowRight' && currentPositionY < width){
+			 	hunter.style.left = (currentPositionY + 10) + 'px';
+				hunter.src = 'obrazky/panacek-vpravo.png'
+				
+			}
+	 	else if (udalost.key=='ArrowUp'&& hunterX >= '0px'){
+			 	hunter.style.top = (currentPositionX - 10) + 'px';
+				hunter.src = 'obrazky/panacek-nahoru.png'
+				
+			}
+		else if (udalost.key=='ArrowDown'&& currentPositionX < height ){
+				hunter.style.top = (currentPositionX + 10) + 'px';
+				hunter.src = 'obrazky/panacek.png'
+				
+			}
+	 	
+	else {alert('Pro ovládání panáčka použij šipky na klávesnici')
+	}  
+
 }
+
+
+	
+
+
